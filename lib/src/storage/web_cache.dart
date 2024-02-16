c38408fdcfa9b999274b727f78e5ae2856ca6cfe
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-02-17 00:35:43 +0900

    feat: extension type

--- lib/src/storage/web_cache.dart
+++ lib/src/storage/web_cache.dart
@@ -1,24 +1,16 @@
 import 'dart:js_interop';
 
+/// [https://developer.mozilla.org/en-US/docs/Web/API/Window]
 @JS()
-@staticInterop
 external Window get window;
 
-/// see [https://developer.mozilla.org/en-US/docs/Web/API/Window]
-@JS('Window')
-@staticInterop
-class Window {}
-
-extension WindowExtension on Window {
+/// [https://developer.mozilla.org/en-US/docs/Web/API/Window]
+extension type Window(JSObject _) implements JSObject {
   external CacheStorage? get caches;
 }
 
-/// see [https://developer.mozilla.org/en-US/docs/Web/API/Cache]
-@JS('Cache')
-@staticInterop
-class Cache {}
-
-extension CacheExtension on Cache {
+/// [https://developer.mozilla.org/en-US/docs/Web/API/Cache]
+extension type Cache(JSObject _) implements JSObject {
   external JSPromise match(JSString request);
 
   external JSPromise put(JSString request, Response response);
@@ -26,36 +18,26 @@ extension CacheExtension on Cache {
   external JSPromise delete(JSString request);
 }
 
-/// see [https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage]
-@JS('CacheStorage')
-@staticInterop
-class CacheStorage {}
-
-extension CacheStorageExtension on CacheStorage {
+/// [https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage]
+extension type CacheStorage(JSObject _) implements JSObject {
   external JSPromise open(JSString cacheName);
 }
 
-/// see [https://developer.mozilla.org/en-US/docs/Web/API/Response]
-@JS('Response')
-@staticInterop
-class Response {
+/// [https://developer.mozilla.org/en-US/docs/Web/API/Response]
+extension type Response._(JSObject _) implements JSObject {
   external factory Response([
     JSAny? body,
-    ResponseInit init,
+    ResponseOptions options,
   ]);
-}
 
-extension ResponseExtension on Response {
   external JSPromise text();
 
   external JSPromise arrayBuffer();
 }
 
-@JS()
-@staticInterop
-@anonymous
-class ResponseInit {
-  external factory ResponseInit({
+/// [https://developer.mozilla.org/en-US/docs/Web/API/Response/Response#options]
+extension type ResponseOptions._(JSObject _) implements JSObject {
+  external factory ResponseOptions({
     JSAny? headers,
   });
 }
