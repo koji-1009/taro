132aa0459038407e4e1d28604c78d550fc7dce0c
koji-1009 <koji-1009@users.noreply.github.com>
2024-03-20 12:40:32 +0000

    deploy: be1f1ce1042bb87fdbca8457b31252fb73d43589

--- main.dart.js
+++ main.dart.js
@@ -25011,7 +25011,7 @@ A.aaj.prototype={
 $1(a){var s=A.dW().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/04817c99c9fd4956f27505204f7e344335810aed/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/2e4ba9c6fb499ccd4e81420543783cc7267ae406/":s)+a},
 $S:83}
 A.aav.prototype={
 $1(a){this.a.remove()
