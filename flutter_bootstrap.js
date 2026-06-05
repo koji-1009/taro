c610ce2adf861f97e5d96ea69d8d0153cbdfe1fc
koji-1009 <koji-1009@users.noreply.github.com>
2026-06-05 23:11:23 +0000

    deploy: b4b18e9ea82c219ca8ee2b20f4a48728cfb66777

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d937","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27da0","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4003778115" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1908919648" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
